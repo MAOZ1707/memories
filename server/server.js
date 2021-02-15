bbaa5117cc11dae68d8f5287d0105f99698a6a3e
Maoz.C <36860867+MAOZ1707@users.noreply.github.com>
2021-02-15 21:10:59 +0200

    start client

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const chalk = require("chalk");
-const app = require("./app");
-dotenv.config({ path: "./config.env" });
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const chalk = require('chalk');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 app.use(cors());
 app.use(cors({ origin: true, credentials: true }));
 
 let dbUrl =
-	"mongodb+srv://maoz:3Cv2an1IOlzRNDIm@cluster0.klzzy.mongodb.net/memories?retryWrites=true&w=majority";
+	'mongodb+srv://maoz:3Cv2an1IOlzRNDIm@cluster0.klzzy.mongodb.net/memories?retryWrites=true&w=majority';
 
 if (process.env.DB_URL) {
 	dbUrl = process.env.DB_URL;
@@ -22,9 +23,9 @@ mongoose
 		useFindAndModify: false,
 	})
 	.then(() => {
-		console.log(chalk.green.bold("DB connection successful!!"));
+		console.log(chalk.green.bold('DB connection successful!!'));
 	})
-	.catch((err) => console.log(err, chalk.red.bold("DB connection failed!!")));
+	.catch((err) => console.log(err, chalk.red.bold('DB connection failed!!')));
 
 // server
 let port = 4000;
@@ -33,5 +34,5 @@ if (process.env.PORT) {
 }
 
 app.listen(port, () => {
-	console.log(chalk.magentaBright("App running !!"));
+	console.log(chalk.magentaBright('App running !!'));
 });
